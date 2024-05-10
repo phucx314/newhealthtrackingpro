8f76980c4073ada88ab951b1d3084256260c3170
phucx314 <tavinhphuc123@gmail.com>
2024-05-10 10:38:27 +0700

    update logic moi, chi hien thi ly moi khi da filled ly cu, hien thi toi da 20 ly nuoc

--- lib/components/water_intake.dart
+++ lib/components/water_intake.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../colors/color_set.dart';
@@ -14,26 +12,12 @@ class WaterIntake extends StatefulWidget {
 
 class _WaterIntakeState extends State<WaterIntake> {
   int waterPerCup = 400; // Lượng nước mỗi ly, khởi tạo ban đầu là 400ml
-  List<Map<String, dynamic>> waterConsumed = List.generate(10, (index) => {'filled': false, 'amount': 0});
+  List<int> waterConsumed = [0]; // Chỉ có một ly nước ban đầu
 
   // Hàm tính lượng nước đã uống
   double calculateWaterIntake() {
     // Tính tổng số ml nước đã uống
-    return waterConsumed.where((cup) => cup['filled']).fold(0, (sum, cup) => sum + cup['amount']);
-  }
-
-  // Hàm để cập nhật số lượng ly nước và lượng nước trong mỗi ly khi thay đổi lượng nước mỗi ly
-  void updateWaterCupsCount() {
-    setState(() {
-      int newCupCount = (2000 ~/ waterPerCup) + 1;
-      if (newCupCount > waterConsumed.length) {
-        // Tăng số lượng phần tử trong danh sách waterConsumed nếu cần
-        waterConsumed.addAll(List.generate(newCupCount - waterConsumed.length, (index) => {'filled': false, 'amount': 0}));
-      } else if (newCupCount < waterConsumed.length) {
-        // Giảm số lượng phần tử trong danh sách waterConsumed nếu cần
-        waterConsumed.removeRange(newCupCount, waterConsumed.length);
-      }
-    });
+    return waterConsumed.reduce((value, element) => value + element).toDouble();
   }
 
   // Hàm để tăng lượng nước mỗi ly
@@ -45,8 +29,6 @@ class _WaterIntakeState extends State<WaterIntake> {
       if (waterPerCup > 500) {
         waterPerCup = 500;
       }
-      // Cập nhật số lượng ly nước và lượng nước trong mỗi ly
-      updateWaterCupsCount();
     });
   }
 
@@ -59,52 +41,30 @@ class _WaterIntakeState extends State<WaterIntake> {
       if (waterPerCup < 100) {
         waterPerCup = 100;
       }
-      // Cập nhật số lượng ly nước và lượng nước trong mỗi ly
-      updateWaterCupsCount();
-    });
-  }
-
-  // Hàm để điều chỉnh lượng nước trong mỗi ly đã được điền khi thay đổi lượng nước mỗi ly
-  void adjustFilledCupAmount() {
-    setState(() {
-      waterConsumed.forEach((cup) {
-        if (cup['filled']) {
-          cup['amount'] = waterPerCup;
-        }
-      });
     });
   }
 
   // Hàm kiểm tra xem có thể chuyển đổi trạng thái của một ly nước được không
   bool canToggleWaterConsumed(int index) {
-    if (index == 0) {
-      // Nếu là ly nước đầu tiên, chỉ cần kiểm tra ly kế tiếp
-      return !waterConsumed[index + 1]['filled'];
-    } else if (index == waterConsumed.length - 1) {
-      // Nếu là ly nước cuối cùng, chỉ cần kiểm tra ly trước đó
-      return waterConsumed[index - 1]['filled'];
-    } else {
-      // Kiểm tra ly trước và ly sau
-      return waterConsumed[index - 1]['filled'] && !waterConsumed[index + 1]['filled'];
-    }
+    return waterConsumed[index] == 0; // Chỉ có thể điền nước nếu ly trống
   }
 
   void toggleWaterConsumed(int index) {
-    if (canToggleWaterConsumed(index)) {
-      setState(() {
-        waterConsumed[index]['filled'] = !waterConsumed[index]['filled'];
-        if (waterConsumed[index]['filled']) {
-          // Nếu điền nước vào ly, điều chỉnh lượng nước trong ly này
-          waterConsumed[index]['amount'] = waterPerCup;
+    setState(() {
+      // Kiểm tra xem ly nước có được điền nước không và không phải ly đầu tiên
+      if (waterConsumed[index] != 0 && index != 0) {
+        waterConsumed[index] = 0; // Rút nước ra khỏi ly
+      } else if (canToggleWaterConsumed(index) && waterConsumed.length <= 20) {
+        // Thêm một ly nước mới và điền nước cho ly hiện tại
+        waterConsumed[index] = waterPerCup;
+        // Kiểm tra nếu đã điền đủ 20 ly thì không thêm nữa
+        if (waterConsumed.length < 20) {
+          waterConsumed.add(0); // Thêm một ly nước mới trống
         }
-      });
-    }
+      }
+    });
   }
 
-  // Getter để lấy lượng nước mỗi ly
-  int getWaterPerCup() {
-    return waterPerCup;
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -119,7 +79,7 @@ class _WaterIntakeState extends State<WaterIntake> {
         padding: const EdgeInsets.all(25),
         child: Column(
           children: [
-            // chỉnh lượng nước mỗi ly
+            // chỉnh lượgn nước mỗi ly
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
@@ -133,7 +93,7 @@ class _WaterIntakeState extends State<WaterIntake> {
 
                 // lượng nước mỗi ly
                 Text(
-                  '${getWaterPerCup()} ml per cup',
+                  '$waterPerCup ml per cup',
                   style: TextStyle(color: htaPrimaryColors.shade500,),
                 ),
 
@@ -168,7 +128,7 @@ class _WaterIntakeState extends State<WaterIntake> {
                     ),
                     child: Center(
                       child: Image(
-                        image: waterConsumed[index]['filled']
+                        image: waterConsumed[index] != 0
                             ? AssetImage('assets/icons/cup_filled.png')
                             : AssetImage('assets/icons/cup_empty.png'),
                       ),
